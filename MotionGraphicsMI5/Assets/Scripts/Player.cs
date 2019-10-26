12ac15e31b3faa956e3083a6fb51e8a220db1e8f
SimonRaknak <49533286+SimonRaknak@users.noreply.github.com>
2019-10-26 15:25:24 +0200

    Bewegung gefixt

--- MotionGraphicsMI5/Assets/Scripts/Player.cs
+++ MotionGraphicsMI5/Assets/Scripts/Player.cs
@@ -42,13 +42,13 @@ public class Player : MonoBehaviour
 
 	//Player Movement
 		if(Input.GetKey(KeyCode.W))
-			transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+			transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
 		if(Input.GetKey(KeyCode.S))
-			transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+			transform.position += Vector3.back * movementSpeed * Time.deltaTime;
 		if(Input.GetKey(KeyCode.A))
-			transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+            transform.position += Vector3.left* movementSpeed * Time.deltaTime;
 		if(Input.GetKey(KeyCode.D))
-			transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
 
 
 		//Shooting
