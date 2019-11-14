fd40df23e437e955c44c4f62ddfa557914f621e9
Divikey <49533256+Divikey@users.noreply.github.com>
2019-11-14 10:31:53 +0100

    Update Bullet.cs

--- MotionGraphicsMI5/Assets/Scripts/Bullet.cs
+++ MotionGraphicsMI5/Assets/Scripts/Bullet.cs
@@ -12,6 +12,7 @@ public class Bullet : MonoBehaviour
     public float damage;
     Vector3 mousePosition = new Vector3();
     Vector3 hitpoint;
+    
 
     void Start()
     {
@@ -19,16 +20,30 @@ public class Bullet : MonoBehaviour
     }
     void Update()
     {
+        
         float hitDist = 0.0f;
         if (Input.GetMouseButtonDown(0))
         {
             mousePosition = UnityEngine.Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, (transform.position - UnityEngine.Camera.main.transform.position).magnitude));
             Debug.Log(mousePosition);
+            shoot();
             
         }
-     
-       
+
+
         //Vorwärtsbewegung der "Kugel"
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDist = 0.0f;
+        if (playerPlane.Raycast(ray, out hitDist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5f * Time.deltaTime);
+        }
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         maxDist += 1* Time.deltaTime;
 
