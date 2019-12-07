e2b7e138f51f5799b71a7af702f11403627d77a9
pokekasper <49149903+pokekasper@users.noreply.github.com>
2019-12-07 13:33:57 +0100

    multiplayerupdate
    its sync working

---
+++ MotionGraphicsMI5/Assets/Scripts/PlayerObjext.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObjext : NetworkBehaviour
+{
+
+	public GameObject PlayerUnitPrefab;
+	GameObject myPlayerUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		// is this my own local playerobject
+		if(isLocalPlayer == false)
+		{
+			//this object belongs to another player
+			return;
+		}
+		
+		CmdSpawnMyUnit();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Remember: update runs on everyons computer, wherether or not they own this
+		//particular player object
+		if( isLocalPlayer == false)
+		{
+			return;
+		}
+		
+    }
+
+	// Commands are special functions that only get executed on the server
+
+	[Command]
+	void CmdSpawnMyUnit()
+	{
+		//We are guaranteed to be on the server right now
+		GameObject go = Instantiate(PlayerUnitPrefab);
+
+		myPlayerUnit = go;
+
+		//go.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
+
+		//now that the object exists on the server, propagate it to all
+		// the clients (and also wire up the NetworkIdentitty)
+		NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+	}
+
+	[Command]
+	void CmdMoveUnitUp()
+	{
+		if(myPlayerUnit == null)
+		{
+			return;
+		}
+
+		myPlayerUnit.transform.Translate(0,1,0);
+	}
+
+}
---
+++ MotionGraphicsMI5/Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// A Playerunit is a unit controlled by a player
+// this could be a character in an fps, a zergling in a rts
+// or a scout in a tbs
+
+public class PlayerUnit : NetworkBehaviour
+{
+	public GameObject playerObj;
+	public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //How do i verfiy that i am allowed to mess around with this objecT?
+		if( hasAuthority == false)
+		{
+			return;
+		}
+		
+		
+		
+		
+		//Player Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Rotation
+            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, targetRotation, 100f * Time.deltaTime);
+            //Bewegung
+            transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
+            
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //Rotation
+            Quaternion targetRotation = Quaternion.LookRotation(Vector3.back);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, targetRotation, 100f * Time.deltaTime);
+            //Bewegung
+            transform.position += Vector3.back * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //Rotation
+            Quaternion targetRotation = Quaternion.LookRotation(Vector3.left);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, targetRotation, 100f * Time.deltaTime);
+            //Bewegung
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+            Debug.Log("player.transform.rotation: "+playerObj.transform.rotation);
+        }   
+        if (Input.GetKey(KeyCode.D))
+        {
+            //Rotation
+            Quaternion targetRotation = Quaternion.LookRotation(Vector3.right);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, targetRotation, 100f * Time.deltaTime);
+            //Bewegung
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        }
+		
+    }
+}
