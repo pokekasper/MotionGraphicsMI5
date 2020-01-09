f045912ca720400b1edf9c6d7d0049f0224ff76a
SimonRaknak <49533286+SimonRaknak@users.noreply.github.com>
2020-01-09 19:43:26 +0100

    MapDestruction

---
+++ MotionGraphicsMI5/Assets/Scripts/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public Collider trigger;
+    private float deltaTime = 0;
+    private float time = 400;
+    private bool outside = false;
+    private void OnTriggerExit(Collider other)
+    {
+        if(other == trigger)
+        {
+            Debug.Log(gameObject);
+            Debug.Log("Selected Trigger");
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            outside = true;
+        }
+        
+        
+    }
+    private void Update()
+    {
+        if (outside)
+        {
+            deltaTime++;
+            if (deltaTime > time)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    
+
+}
---
+++ MotionGraphicsMI5/Assets/Scripts/cirleCenter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cirleCenter : MonoBehaviour
+{
+    public SphereCollider coll;
+    public float speed;
+    public float endRadius;
+    
+    
+    void Update()
+    {
+        if (coll.radius > endRadius)
+        {
+            coll.radius -= speed;
+        }
+       
+       
+        //coll.size = new Vector3(coll.size.x - speed, coll.size.y, coll.size.z - speed);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(new Vector3(0, 10, 0), coll.radius);
+        
+    }
+}
