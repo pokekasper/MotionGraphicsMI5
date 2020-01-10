44fc8a7de3d0bed86f327430dcef69fc5c1859af
pokekasper <49149903+pokekasper@users.noreply.github.com>
2020-01-10 15:11:09 +0100

    update_monster

---
+++ MotionGraphicsMI5/Assets/Scripts/Player_NetworkMonster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class Player_NetworkMonster : NetworkBehaviour {
+
+    public GameObject firstPersonCharacter;
+	public GameObject test;
+
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Player>().enabled = true;
+		GetComponent<TestAnimation>().enabled = true;
+		test.SetActive(true);
+		//camera.GetComponent<Camera>().enabled = true;
+		GetComponent<NetworkAnimator>().SetParameterAutoSend(0,true);
+		GameObject.Find("Main Camera").SetActive(false);
+        firstPersonCharacter.SetActive(true);
+		
+    }
+}
