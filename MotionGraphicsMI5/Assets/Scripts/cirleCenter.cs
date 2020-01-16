db1364071f2992d708ba5283af2d675a886f4e66
SimonRaknak <49533286+SimonRaknak@users.noreply.github.com>
2020-01-16 23:46:00 +0100

    exploding stone fix

--- MotionGraphicsMI5/Assets/Scripts/cirleCenter.cs
+++ MotionGraphicsMI5/Assets/Scripts/cirleCenter.cs
@@ -13,7 +13,7 @@ public class cirleCenter : MonoBehaviour
     void Update()
     {
 		array= GameObject.FindGameObjectsWithTag("PlayerHolder");
-		if(array.Length == 1)
+		if(array.Length == 2)
 		{
 			if (coll.radius > endRadius)
 			{
