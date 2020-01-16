2a2083fa558267f32ee7bd0e0e45d55d104fbe78
pokekasper <49149903+pokekasper@users.noreply.github.com>
2020-01-16 12:24:25 +0100

    update_characterMenuSound

---
+++ MotionGraphicsMI5/Assets/Scripts/btnFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnFX : MonoBehaviour
+{
+    public AudioSource myFx;
+	public AudioClip clickFx;
+	
+	public void ClickSound()
+	{
+		myFx.PlayOneShot (clickFx);
+	}
+	public void StopSound()
+	{
+		if (myFx.isPlaying)
+		{
+			myFx.Stop();
+		}
+	}
+}
