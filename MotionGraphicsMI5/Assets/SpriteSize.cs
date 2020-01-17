74715a8f285fb96df86eb57a139821dd2343f358
SimonRaknak <49533286+SimonRaknak@users.noreply.github.com>
2020-01-17 02:28:09 +0100

    death zone marker

---
+++ MotionGraphicsMI5/Assets/SpriteSize.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSize : MonoBehaviour
+{
+    public SphereCollider coll;
+    private float startRadius;
+    private Vector3 startLocalScale;
+    private float radius;
+    private float verhältnisRadius;
+
+    private void Start()
+    {
+        startRadius = coll.radius;
+        startLocalScale = transform.localScale;
+    }
+    void Update()
+    {
+        radius = coll.radius;
+        verhältnisRadius = radius / startRadius;
+
+        transform.localScale = startLocalScale * verhältnisRadius; 
+    }
+}
